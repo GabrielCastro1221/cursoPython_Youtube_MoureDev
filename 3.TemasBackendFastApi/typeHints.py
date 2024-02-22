98fab6632377d04c1d5090bf962e1e534fbca358
Gabriel Castro Ramirez <141979377+GabrielCastro1221@users.noreply.github.com>
2024-02-21 23:48:10 -0300

    delete 3

--- 3.TemasBackendFastApi/typeHints.py
+++
@@ -1,8 +0,0 @@
-#TYPE HINTS
-
-#se le da un tipo de daro a la variable pa mayor compresion en el backend
-my_typed_variable: str = "my typed string variable"
-print(type(my_typed_variable))
-
-my_typed_varibale_2: int = 5
-print(type(my_typed_varibale_2))
